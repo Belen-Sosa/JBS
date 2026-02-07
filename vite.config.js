cd10e57a9888f1a5e6d395a752744a05ebf875a8
Belen-Sosa <sosabelen597@gmail.com>
2026-02-07 09:16:20 -0300

    fix configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
-  base: "/JBS/"
-})
+  base: "/JBS/",
+});
